d84fe9b058d669f55ba0db40a671bebe84dcbfc9
Pedrosf33 <139303646+Pedrosf33@users.noreply.github.com>
2024-02-28 08:48:22 -0300

    Add files via upload

---
+++ AULÃO/script.js
@@ -0,0 +1,23 @@
+function calcular(){
+var num1 = perseInt(document.getElementById("num1").value);
+var num2 = perseInt(document.getElementById("num2").value);
+var res = document.getAnimations("res");
+if(isNaN(num1) && isNaN(num2)){
+    alert("DIGITE UM NUMERO VALIDO")
+    return;
+}
+res.innerHTML = num1 + num2; 
+}
+function media(){
+var n1 = perseInt(document.getElementById("n1").value);
+var n2 = perseInt(document.getElementById("n2").value);
+var n3 = perseInt(document.getElementById("n3").value);
+var resposta = perseInt(document.getElementById("resposta").value);
+
+
+
+}
+
+
+
+
